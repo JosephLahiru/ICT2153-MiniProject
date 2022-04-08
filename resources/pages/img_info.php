3612bbeed9b289b8d4910e86424b60acc748af4e
theAlphaGuardian <44818405+JosephLahiru@users.noreply.github.com>
2022-04-08 12:30:01 +0530

    minor

--- resources/pages/img_info.php
+++ resources/pages/img_info.php
@@ -62,25 +62,25 @@
 						break;
 					}
 				}
-				$view = $row['views'];
-				echo "<div>Current Views : $view</div>";
+				//$view = $row['views'];
+				//echo "<div>Current Views : $view</div>";
 				?>
 	<!-- 			<form action="" method="post">
 					<input type="submit" name="rank_up" value="Vote Up">
 				</form> -->
 				<?php
-					$new_view = $view+1;
-					$img_id = $_GET['img_id'];
+					// $new_view = $view+1;
+					// $img_id = $_GET['img_id'];
 
-					$view_update_sql = "UPDATE images SET views=$new_view WHERE img_id=$img_id;";
-					if ($conn->query($view_update_sql) === TRUE) {
-						// echo "Record updated successfully";
-						// sleep(2);
-						// header( 'Location: img_info.php?img_id=$img_id' );
+					// $view_update_sql = "UPDATE images SET views=$new_view WHERE img_id=$img_id;";
+					// if ($conn->query($view_update_sql) === TRUE) {
+					// 	// echo "Record updated successfully";
+					// 	// sleep(2);
+					// 	// header( 'Location: img_info.php?img_id=$img_id' );
 
-					} else {
-						echo "Error: " . $sql . "<br>" . $conn->error;
-					}
+					// } else {
+					// 	echo "Error: " . $sql . "<br>" . $conn->error;
+					// }
 				// if(isset($_POST['rank_up'])){
 					// $new_rank = $rank+1;
 					// $img_id = $_GET['img_id'];
@@ -100,7 +100,15 @@
 				echo "<p class='status error'>Image(s) not found...</p>";
 			}
 		?>
-		<div align="center"><a href="payment_portal.php"><button class="buy_button">Buy Now</button></a><button class="cart_button">Add To Cart</button></div>
+		<form action="" method="post">
+			<div align="center"><a href="payment_portal.php"><button class="buy_button">Buy Now</button></a><!-- <button class="cart_button">Add To Cart</button> --><input class="cart_button" type="submit" name="cart" value="Add To Cart"></div>
+		</form>
+
+		<?php
+			if(isset($_POST['cart'])){
+				echo "<script>alert('added to cart successfully');</script>";
+			}
+		?>
 	</div>
 </body>
 </html>
