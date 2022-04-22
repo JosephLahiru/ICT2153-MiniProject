fbfed544a39fc1dcc9e731abf9397f1035d58719
theAlphaGuardian <44818405+JosephLahiru@users.noreply.github.com>
2022-04-22 22:40:32 +0530

    stopped at user_account 78

--- resources/pages/user_account.php
+++ resources/pages/user_account.php
@@ -46,13 +46,42 @@
 		}
 
 		//user details area
+		$sql = "SELECT firstname, lastname, gmail, address FROM user";
+		$result = mysqli_query($conn, $sql);
+		$data = mysqli_fetch_assoc($result);
+
+		$firstname = $data['firstname'];
+		$lastname = $data['lastname'];
+		$gmail = $data['gmail'];
+		$address = $data['address'];
+
+		echo "<h2 class='topic'>User Data</h2>";
+		echo "<table border='1' id='user'>";
+		echo "<tr><td>First Name</td><td>$firstname</td></tr>";
+		echo "<tr><td>Last Name</td><td>$lastname</td></tr>";
+		echo "<tr><td>Gmail</td><td>$gmail</td></tr>";
+		echo "<tr><td>Address</td><td>$address</td></tr>";
+		echo "</table>";
 
 		//owned pictures area -> for sale : checkbox
+		$sql = "SELECT img_id, created, topic, owned FROM images WHERE owned=$logged_user_id";
+		$result = mysqli_query($conn, $sql);
+
+		if(!empty($result)){
+			$count = mysqli_num_rows($result);
+			$data = mysqli_fetch_assoc($result);
+
+			// foreach ($data as $key => $value) {
+			// 	echo "$key -> $value <br>";
+			// }
+
+			print_r($data);
+		}
 
 		//added pictures area : artist only
 
 		if($user_type=='artist'){
-			echo "<div align='center'><a href='list_item.php'><button class='list_button'>List item</button></a></div>";
+			echo "<br><div align='center'><a href='list_item.php'><button class='list_button'>List item</button></a></div>";
 		}
 	?>
 </body>
